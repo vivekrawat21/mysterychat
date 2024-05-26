cffae244320a52711b9133f8f806d1bc7b17c586
vivekrawat21 <vivekrwt2111@gmail.com>
2024-05-26 23:42:09 +0530

    chore: Update npm dependency for bcryptjs to version 2.4.3

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,7 +3,7 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import bcrypt from 'bcryptjs';
 import dbConnect from '@/lib/dbConnect';
 import UserModel from '@/model/User';
-
+// 
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
